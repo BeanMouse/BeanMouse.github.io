04f3c461eaa86f474bb92580fc957e75b9dcceda
leejuhi <anim0203@naver.com>
2025-07-19 20:32:39 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // ✅ 이거 꼭 필요함!
+  },
 };
 
 module.exports = nextConfig;
